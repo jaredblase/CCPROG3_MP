c8d540092d8f7748e548bc9319e1f371cb6873c0
Jared Sy <jaredblase@gmail.com>
2020-08-02 23:18:33 +0800

    Completed Javadoc

--- src/Menu.java
+++ src/Menu.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 
+/**
+ * This class is used for generating menus and retrieving an answer from the user.
+ * @author Pua, Gabriel
+ * @author Sy, Jared
+ * @version 1.0
+ */
 public class Menu {
+    /**
+     * Displays the header of the menu before showing all the menu options passed
+     * to the method in a numbered order.
+     * @param header the header of the menu to be printed i.e. Main Menu.
+     * @param options a variable argument to store the menu options in an array.
+     * @return the option number chosen
+     */
     public static int display(String header, String...options) {
         int ctr = 1;
 
@@ -14,6 +27,12 @@ public class Menu {
         return getAnswer(options.length); //return option input
     }
 
+    /**
+     * Obtains an integer by means of the Scanner class and also performs
+     * exception handling in case an invalid option is input by the user.
+     * @param max indicates the number of menu options available
+     * @return a number representing the chosen option of the user
+     */
     private static int getAnswer(int max) {
         int opt;
         Scanner input = new Scanner(System.in);
@@ -22,8 +41,9 @@ public class Menu {
             System.out.print("Option: ");
             try {
                 opt = input.nextInt(); // get input
-                if (opt < 1 || opt > max) //invalid option input
+                if (opt < 1 || opt > max) {//invalid option input
                     throw new Exception();
+                }
             } catch (Exception e) {
                 System.out.println("Invalid option!\n");
                 opt = max + 1; //set opt to loop again
