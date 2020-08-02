ae0e9eb25b86f32e9dea5d2ef7e67ab10c23c39f
Jared Sy <jaredblase@gmail.com>
2020-08-03 00:46:05 +0800

    Created GovOfficial Class

---
+++ src/GovOfficial.java
@@ -0,0 +1,31 @@
+public class GovOfficial extends Citizen {
+
+    private GovOfficial(Citizen citizen) {
+        super(citizen);
+    }
+
+    @Override
+    public void showMenu() {
+        Menu.display("Main Menu", "Show Analytics");
+    }
+
+    private void showAnalytics() {
+
+    }
+
+    private void showUnassigned() {
+
+    }
+
+    private void createGov() {
+
+    }
+
+    private void createTrace() {
+
+    }
+
+    private void terminateAcc() {
+
+    }
+}
