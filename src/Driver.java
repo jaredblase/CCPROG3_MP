e1561f81d4dd506054d234ef6b28eec43d2ccbcc
Jared Sy <jaredblase@gmail.com>
2020-08-05 10:54:35 +0800

    Added the driver code

---
+++ src/Driver.java
@@ -0,0 +1,24 @@
+
+
+public class Driver {
+    public static void main(String[] args) {
+//        int opt;
+//
+//        do {
+//            opt = Menu.display("Main", "Register", "Login", "Exit");
+//            switch (opt) {
+//                case 1 -> User.register();
+//                case 2 -> User.login();
+//            }
+//        } while (opt != 3);
+//
+//        System.out.println("Terminating program...");
+        User.setPassword("Hello");
+    }
+
+//    private static void login() {
+//
+//    }
+
+
+}
