cc24c4076588e623c05523266a59adc37d6711bc
Jared Sy <jaredblase@gmail.com>
2020-08-07 00:32:09 +0800

    Added isChanged field, implemented updateInfo() and logOut().

--- src/Citizen.java
+++ src/Citizen.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * This class holds the information of a citizen and methods available for them.
@@ -17,16 +20,19 @@ public class Citizen {
     private ArrayList<Visit> visitRec;
     private boolean isPositive;
     private boolean maybePositive;
+    private boolean isChanged;
     protected static String[] menuOptions = {"Check in", "Report positive", "Update profile information", "Logout"};
+    private static final String[] updateOptions = {"Name", "Home Address", "Office Address", "Phone Number",
+            "E-Mail", "Password", "Back to User Menu"};
 
     /**
      *
-     * @param name
-     * @param homeAddress
-     * @param officeAddress
-     * @param phoneNumber
-     * @param email
-     * @param username
+     * @param name the Name of object containing the name of the user
+     * @param homeAddress the home address of the user
+     * @param officeAddress the office address of the user
+     * @param phoneNumber the phone number of the user
+     * @param email the email address of the user
+     * @param username the username of the user
      */
     public Citizen(Name name, String homeAddress, String officeAddress, String phoneNumber,
                    String email, String username) {
@@ -39,6 +45,7 @@ public class Citizen {
         visitRec = new ArrayList<>();
         isPositive = false;
         maybePositive = false;
+        isChanged = false;
     }
 
     protected String getUsername() {
@@ -54,7 +61,9 @@ public class Citizen {
             chooseMenu(opt);
         } while (opt != 4);
 
-        logOut();
+        if(isChanged) {
+            logOut();
+        }
     }
 
     protected void chooseMenu(int opt) {
@@ -80,10 +89,52 @@ public class Citizen {
     }
 
     private void updateInfo() {
-        Menu.display("Update Information Menu", "Name");
+        int opt, max = updateOptions.length;
+
+        do {
+            opt = Menu.display("Update Information", updateOptions);
+            if (opt != max) {
+                if (opt == 1) {
+                    name.changeName();
+                } else if (opt == max - 1) {
+                    User.setPassword(this.username);
+                } else {
+                    Scanner input = new Scanner(System.in);
+                    System.out.print("New " + updateOptions[opt - 1] + ": ");
+                    String str = input.nextLine();
+
+                    switch (opt) {
+                        case 2 -> this.homeAddress = str;
+                        case 3 -> this.officeAddress = str;
+                        case 4 -> this.phoneNumber = str;
+                        case 5 -> this.email = str;
+                    }
+                }
+                System.out.println(updateOptions[opt - 1] + " has been updated!\n");
+                isChanged = true;
+            }
+        } while (opt != max);
     }
 
     protected void logOut() {
+        String pass = null;
+
+        try (Scanner input = new Scanner(new File(username + ".act"))) {
+            pass = input.nextLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try (FileWriter writer = new FileWriter(username + ".act", false)) {
+            writer.write(pass + "\n");
+            writer.write(name.toString() + "\n");
+            writer.write("HOME:" + homeAddress + "\n");
+            writer.write("OFFICE:" + officeAddress + "\n");
+            writer.write("PHONE:" + phoneNumber + "\n");
+            writer.write("EMAIL:" + email + "\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
