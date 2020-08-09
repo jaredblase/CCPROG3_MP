ed892a5a00c43d78441114dda51d1e7d7b8af5a3
GabPua <gabriel_pua@dlsu.edu.ph>
2020-08-09 23:28:50 +0800

    Added Case class

---
+++ src/Case.java
@@ -0,0 +1,33 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Case {
+    private final int CASE_NUM;
+    private final String USERNAME;
+    private final Calendar REPORT_DATE;
+    private String tracer;
+    private char status;
+    private static int ctr = 0;
+    private static final SimpleDateFormat format = new SimpleDateFormat(" MM,dd,yyyy ");
+
+    public Case(String username, Calendar date) {
+        this.CASE_NUM = ++ctr;
+        this.USERNAME = username;
+        this.REPORT_DATE = date;
+        this.tracer = "000";
+        this.status = 'P';
+    }
+
+    public void setTracer(String tracer) {
+        this.tracer = tracer;
+    }
+
+    public void setStatus(char status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return CASE_NUM + " " + USERNAME + format.format(REPORT_DATE.getTime()) + tracer + " " + status;
+    }
+}
