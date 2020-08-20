9ea65e5a8971ecec118ade77b400fe26b000d50c
Jared Sy <jaredblase@gmail.com>
2020-08-20 14:41:23 +0800

    Rearranged methods and completed javadocs

--- src/UserSystem.java
+++ src/UserSystem.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 /**
  * This class handles everything that deals with any actions of a user outside of his account
  * such as registering, login, loading and saving of users to the master list, and changing of
- * passwords-since they are not loaded into the memory during the session.
+ * passwords since they are not loaded into the memory during the session.
  * @author Gabriel Pua
  * @author Jared Sy
- * @version 1.0
+ * @version 1.1
  */
-public abstract class UserSystem {
+public class UserSystem {
     /** All the registered usernames */
     private static ArrayList<String> usernames;
     /** The roles of the registered users */
@@ -24,6 +24,70 @@ public abstract class UserSystem {
     private static int nTracers;
 
     /**
+     * Checks if the username received is in the master list.
+     * @param username the name to be checked in the master list.
+     * @return -1 if not found, greater than or equal to 0 otherwise.
+     */
+    public static int getIndexOf(String username) {
+        return usernames.indexOf(username);
+    }
+
+    /**
+     * Returns the role of specific user in the system given his index.
+     * @param index the index of a specific user in the system.
+     * @return the role of the user.
+     */
+    public static String getRoleOf(int index) {
+        return roles.get(index);
+    }
+
+    /**
+     * Returns the number of registered users.
+     * @return the number of registered users in the system.
+     */
+    public static int getNumUsers() {
+        return usernames.size();
+    }
+
+    /**
+     * Returns the user given a specified username. If the username
+     * is not found in the master list, it returns null.
+     * @param username the name to be checked in the master list.
+     * @return the user with the specified username if found, null otherwise.
+     */
+    public static Citizen getUser(String username) {
+        int index = getIndexOf(username);
+        if (index != -1)
+            return users.get(index);
+        else
+            return null;
+    }
+
+    /**
+     * Returns the visit records of each user.
+     * @return the visit records of each user.
+     */
+    public static ArrayList<ArrayList<Visit>> getRecords() {
+        return records;
+    }
+
+    /**
+     * Returns the list of cases.
+     * @return the list of cases.
+     */
+    public static ArrayList<Case> getCases() {
+        return cases;
+    }
+
+    /**
+     * Returns the number of registered tracers.
+     * @return the number of registered tracers in the system.
+     */
+    public static int getNumTracers() {
+        return nTracers;
+    }
+
+    /**
      * Obtains the input username then calls register(username) to handle
      * the rest of the registration process.
      */
@@ -36,12 +100,14 @@ public abstract class UserSystem {
     }
 
     /**
-     * Handles the registration of a new user given a username
-     * @param username the desired username of the account to created
+     * Handles the registration of a new user given a username.
+     * @param username the chosen username of the account to created.
      */
     public static void register(String username) {
         Scanner input = new Scanner(System.in);
 
+        // Checks if the username is already taken.
+        // If it is, this block loops and asks for a new input.
         while (getIndexOf(username) != -1 || username.isBlank()) {
             if (getIndexOf(username) != -1) // taken username
                 System.out.println("Username has already been taken!\n");
@@ -81,91 +147,11 @@ public abstract class UserSystem {
                 email, username, password));
 
         System.out.println("----YOU MAY NOW LOGIN WITH YOUR NEW ACCOUNT----");
-
-    }
-
-    /**
-     * Initializes all the ArrayLists and creates the admin account
-     */
-    public static void loadUsers() {
-        usernames = new ArrayList<>();
-        roles = new ArrayList<>();
-        users = new ArrayList<>();
-        records= new ArrayList<>();
-        cases = new ArrayList<>();
-        nTracers = 0;
-
-        usernames.add("Admin2020");
-        roles.add("official");
-        users.add(new GovOfficial(new Citizen(new Name("Admin", "A", "Gov"), "Malacañang",
-                "City Hall", "09328287114", "admin@gov.ph", "Admin2020",
-                "@Dm1n0202")));
-        records.add(new ArrayList<>());
-    }
-
-    /**
-     * Checks if the username received is not in the master list.
-     *
-     * @param username the name to be checked in the master list.
-     * @return -1 if not found, greater than or equal to 0 otherwise.
-     */
-    public static int getIndexOf(String username) {
-        return usernames.indexOf(username);
-    }
-
-    /**
-     * Returns the role of specific user in the system given his index
-     * @param index the index of a specific user in the system
-     * @return the role of a user
-     */
-    public static String getRoleOf(int index) {
-        return roles.get(index);
-    }
-
-    /**
-     * Sets a new role for a specific user in the system given his index
-     * @param index the index of a specific user in the system
-     * @param role the new role to be assigned to the user
-     */
-    public static void setRoleOf(int index, String role) {
-        if (roles.get(index).equals("tracer")) { // previous role is tracer
-            nTracers--;
-        }
-
-        roles.set(index, role);
-
-        if (role.equals("tracer")) { // new role is tracer
-            nTracers++;
-        }
-
-        System.out.println("Modification success!");
-    }
-
-    /**
-     * Asks the user to enter a new password and check it for validity. Once valid
-     * return the chosen password.
-     * @return valid password entered by the user.
-     */
-    public static String setPassword() {
-        Scanner input = new Scanner(System.in);
-        String regex = "[\\w\\s]*[\\W\\d][\\w\\s]*";
-        String pass;
-
-        System.out.print("New Password: ");
-        pass = input.nextLine();
-        while(pass.length() < 6 || !pass.replaceAll("\\s+", "").matches(regex)) {
-            System.out.println("Password must contain at least 6 characters including 1 special " +
-                    "character that is not a space!\n");
-            System.out.print("New Password: ");
-            pass = input.nextLine();
-        }
-
-        return pass;
     }
 
     /**
      * Handles the logging in of an existing user. It asks the user to input his username
-     * and password. Once valid, loads everything from the user's text file.
+     * and password. Once valid, loads everything from the user's information in the system.
      * @return a constructed Citizen object with the user's details.
      */
     public static Citizen login() {
@@ -196,6 +182,47 @@ public abstract class UserSystem {
     }
 
     /**
+     * Asks the user to enter a new password and check it for validity.<br>
+     * Once valid, returns the chosen password.
+     * @return valid password entered by the user.
+     */
+    public static String setPassword() {
+        Scanner input = new Scanner(System.in);
+        String regex = "[\\w\\s]*[\\W\\d][\\w\\s]*"; // looks for at least one special character
+
+        System.out.print("New Password: ");
+        String pass = input.nextLine();
+        // Loops and asks for another password while the password is invalid.
+        while(pass.length() < 6 || !pass.replaceAll("\\s+", "").matches(regex)) {
+            System.out.println("Password must contain at least 6 characters including 1 special " +
+                    "character that is not a space!\n");
+            System.out.print("New Password: ");
+            pass = input.nextLine();
+        }
+
+        return pass;
+    }
+
+    /**
+     * Sets a new role for a specific user in the system given his index.
+     * @param index the index of a specific user in the system.
+     * @param role the new role to be assigned to the user.
+     */
+    public static void setRoleOf(int index, String role) {
+        if (roles.get(index).equals("tracer")) { // previous role is tracer
+            nTracers--;
+        }
+
+        roles.set(index, role);
+
+        if (role.equals("tracer")) { // new role is tracer
+            nTracers++;
+        }
+
+        System.out.println("Modification success!");
+    }
+
+    /**
      * Replaces the object in the system with the new and update one.
      * @param citizen the object to replace the one in the system.
      */
@@ -204,56 +231,29 @@ public abstract class UserSystem {
     }
 
     /**
-     * Adds a case to the list of cases in the system
-     * @param positive the case to be added
+     * Adds a case to the list of cases in the system.
+     * @param positive the case to be added.
      */
     public static void addCase(Case positive) {
         cases.add(positive);
     }
 
     /**
-     * Returns the list of cases.
-     * @return the list of cases.
+     * Initializes all the ArrayLists and creates the admin account.
      */
-    public static ArrayList<Case> getCases() {
-        return cases;
-    }
+    public static void loadUsers() {
+        usernames = new ArrayList<>();
+        roles = new ArrayList<>();
+        users = new ArrayList<>();
+        records= new ArrayList<>();
+        cases = new ArrayList<>();
+        nTracers = 0;
 
-    /**
-     * Returns the number of registered tracers.
-     * @return the number of registered tracers in the system.
-     */
-    public static int getNumTracers() {
-        return nTracers;
-    }
-
-    /**
-     * Returns the number of registered users.
-     * @return the number of registered users in the system.
-     */
-    public static int getNumUsers() {
-        return usernames.size();
-    }
-
-    /**
-     * Returns the user given a specified username. If the username
-     * is not found in the master list, it returns null.
-     * @param username the name to be checked in the master list
-     * @return the user with the specified username if found, null otherwise
-     */
-    public static Citizen getUser(String username) {
-        int index = getIndexOf(username);
-        if (index != -1)
-            return users.get(index);
-        else
-            return null;
-    }
-
-    /**
-     * Returns the visit records of each user
-     * @return the visit records of each user
-     */
-    public static ArrayList<ArrayList<Visit>> getRecords() {
-        return records;
+        usernames.add("Admin2020");
+        roles.add("official");
+        users.add(new GovOfficial(new Citizen(new Name("Admin", "A", "Gov"), "Malacañang",
+                "City Hall", "09328287114", "admin@gov.ph", "Admin2020",
+                "@Dm1n0202")));
+        records.add(new ArrayList<>());
     }
 }
