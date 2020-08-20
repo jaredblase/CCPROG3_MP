40571f36e3f64fbd1b505e3880b6f3b4e4796b52
Jared Sy <jaredblase@gmail.com>
2020-08-20 14:40:26 +0800

    Removed SimpleDateFormat attribute, and rearranged the methods. And added a few javadocs

--- src/Case.java
+++ src/Case.java
@@ -1,22 +1,30 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+/**
+ * The Case class...
+ */
 public class Case {
-    /** The case number */
+    /** The case number. */
     private final int CASE_NUM;
-    /** The username of the positive case */
+    /** The username of the positive case. */
     private final String USERNAME;
-    /** The date the case was reported */
+    /** The date the case was reported. */
     private final Calendar REPORT_DATE;
-    /** The username of the contact tracer assigned to the case */
+    /** The username of the contact tracer assigned to the case. */
     private String tracer;
-    /** The status whether the case is already traced or not */
+    /** The status whether the case is already traced or not. */
     private char status;
-    /** The total number of cases */
+    /** The total number of cases. */
     private static int ctr = 0;
-    /** The date format */
-    private static final SimpleDateFormat format = new SimpleDateFormat(" MM,dd,yyyy ");
 
+    /**
+     * Constructs a Case object and assigns the username and date from the
+     * parameters. Case number is automatically assigned and default values are
+     * initialized to the rest of the fields.
+     * @param username the username of the citizen who reported positive.
+     * @param date the date when the user reported positive.
+     */
     public Case(String username, Calendar date) {
         this.CASE_NUM = ++ctr;
         this.USERNAME = username;
@@ -26,71 +34,72 @@ public class Case {
     }
 
     /**
-     * Sets the username of the contact tracer assigned to the case
-     * @param tracer the username of the contact tracer assigned to the case
-     */
-    public void setTracer(String tracer) {
-        this.tracer = tracer;
-    }
-
-    /**
-     * Sets the status whether the case is already traced or not
-     * @param status the status whether the case is already traced or not
-     */
-    public void setStatus(char status) {
-        this.status = status;
-    }
-
-    /**
-     * Returns the username of the contact tracer assigned to the case
-     * @return the username of the contact tracer assigned to the case
-     */
-    public String getTracer() {
-        return tracer;
-    }
-
-    /**
-     * Returns the date the case was reported
-     * @return the date the case was reported
-     */
-    public Calendar getReportDate() {
-        return REPORT_DATE;
-    }
-
-    /**
-     * Returns the case number
-     * @return the case number
+     * Returns the case number.
+     * @return the case number.
      */
     public int getCaseNum() {
         return CASE_NUM;
     }
 
     /**
-     * Returns the total number of cases
-     * @return the total number of cases
+     * Returns the username of the positive case.
+     * @return the username of the positive case.
      */
-    public static int getCount() {
-        return ctr;
+    public String getUsername() {
+        return USERNAME;
     }
 
     /**
-     * Returns the status whether the case is already traced or not
-     * @return the status whether the case is already traced or not
+     * Returns the date the case was reported.
+     * @return the date the case was reported.
+     */
+    public Calendar getReportDate() {
+        return REPORT_DATE;
+    }
+
+    /**
+     * Returns the username of the contact tracer assigned to the case.
+     * @return the username of the contact tracer assigned to the case.
+     */
+    public String getTracer() {
+        return tracer;
+    }
+
+    /**
+     * Returns the status whether the case is already traced or not.
+     * @return the status whether the case is already traced or not.
      */
     public char getStatus() {
         return status;
     }
 
     /**
-     * Returns the username of the positive case
-     * @return the username of the positive case
+     * Returns the total number of cases.
+     * @return the total number of cases.
      */
-    public String getUsername() {
-        return USERNAME;
+    public static int getCount() {
+        return ctr;
+    }
+
+    /**
+     * Sets the username of the contact tracer assigned to the case.
+     * @param tracer the username of the contact tracer assigned to the case.
+     */
+    public void setTracer(String tracer) {
+        this.tracer = tracer;
+    }
+
+    /**
+     * Sets the status whether the case is already traced or not.
+     * @param status the status whether the case is already traced or not.
+     */
+    public void setStatus(char status) {
+        this.status = status;
     }
 
     @Override
     public String toString() {
+        SimpleDateFormat format = new SimpleDateFormat(" MM,dd,yyyy ");
         return CASE_NUM + " " + USERNAME + format.format(REPORT_DATE.getTime()) + tracer + " " + status;
     }
 }
