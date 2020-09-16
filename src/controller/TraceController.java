97b2031c10ff745fbb1dc1a26f02a8637d1b1c02
Jared Sy <jaredblase@gmail.com>
2020-09-17 03:33:31 +0800

    Trace updates

--- src/controller/TraceController.java
+++ src/controller/TraceController.java
@@ -1,4 +1,43 @@
 package controller;
 
-public class TraceController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import model.Case;
+import model.Tracer;
+import model.Visit;
+
+public class TraceController extends Controller {
+    private Tracer tracer;
+    @FXML
+    private ComboBox<Integer> caseNumber;
+    @FXML
+    private TableColumn<Visit, String> visit;
+    @FXML
+    private MenuController menuController;
+
+    @Override
+    public void update() {
+        menuController.setMainController(mainController);
+
+        tracer = (Tracer) mainController.getUserModel();
+        init();
+    }
+
+    private void init() {
+        ObservableList<Integer> caseNums = FXCollections.observableArrayList();
+
+        for(Case i : tracer.getAssigned())
+            caseNums.add(i.getCaseNum());
+
+        caseNumber.setItems(caseNums);
+    }
+
+    @FXML
+    public void handleInformCitizenAction() {
+        // remove case number from choices
+        caseNumber.getItems().removeAll(caseNumber.getValue());
+    }
 }
