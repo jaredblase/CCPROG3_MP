eb71995ed0ffff21bbdbf7fe72507a7c71a790b2
Jared Sy <jaredblase@gmail.com>
2020-09-18 01:18:08 +0800

    Added javadocs

--- src/controller/Registration2Controller.java
+++ src/controller/Registration2Controller.java
@@ -36,6 +36,11 @@ public class Registration2Controller extends Controller {
     @FXML
     private Label invalidPhoneNumber;
 
+    /**
+     * Automatically called when the corresponding fxml file is loaded by FXML loader.
+     * Sets up the behaviour of the invalid message labels.
+     */
+    @FXML
     public void initialize() {
         firstName.setOnKeyPressed(e -> invalidFirstName.setVisible(false));
         middleName.setOnKeyPressed(e -> invalidMiddleName.setVisible(false));
@@ -51,12 +56,20 @@ public class Registration2Controller extends Controller {
 
     }
 
+    /**
+     * Handles the event where the user presses the back button.
+     * Brings the user back to the previous scene.
+     */
     @FXML
     public void handleBackToForm1Action() {
         UserSystem.removeUser();
         mainController.changeScene(MainController.REGISTER_VIEW);
     }
 
+    /**
+     * Handles the event where the user presses the register button.
+     * This will add his information to the user system.
+     */
     @FXML
     public void handleRegisterButtonAction() {
         boolean isValid = true;
